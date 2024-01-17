8fd135a3281a47d6b99b4313f30b728407acb3b1
hawk-s <53600945@fsv.cuni.cz>
2024-01-17 21:25:15 +0100

    attempt to prevent overwriting of json files

--- fctions/C1_json_writer.py
+++ fctions/C1_json_writer.py
@@ -18,3 +18,36 @@ def write_to_json(filename, data):
     except IOError as e:
         # Handle file write errors
         raise IOError(f"An error occurred while writing to {filename}: {e}")
+    
+
+#use in case one wants to prevent overwriting the same file each time the code runs:
+'''
+import json
+import os
+
+def write_to_json(filename, data):
+    """
+    Write data to a JSON file if the file does not already exist.
+
+    Args:
+        filename (str): The name of the JSON file to create.
+        data (object): The data to be written to the JSON file. It should be JSON-serializable.
+
+    Returns:
+        str: Message indicating the result of the operation.
+
+    Raises:
+        IOError: If an error occurs while writing to the file.
+    """
+    if os.path.exists(filename):
+        # File already exists, skip writing
+        return f"File '{filename}' already exists. Skipping file creation."
+
+    try:
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+        return f"File '{filename}' successfully created."
+    except IOError as e:
+        # Handle file write errors
+        raise IOError(f"An error occurred while writing to {filename}: {e}")
+'''
